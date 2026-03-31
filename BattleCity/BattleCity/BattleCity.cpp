5aa4a266cdcd3767a7a8c9a1653b65ade62e0337
Dubovik Pavel <pawii@yandex.ru>
2026-04-01 00:05:47 +0300

    create CMake template project

---
+++ BattleCity/BattleCity/BattleCity.cpp
@@ -0,0 +1,12 @@
+﻿// BattleCity.cpp : Defines the entry point for the application.
+//
+
+#include "BattleCity.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
